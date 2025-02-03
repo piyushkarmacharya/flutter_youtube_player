47c78115018bf220ded0ea093cd7b95d022bf154
piyushkarmacharya <piyushkarmacharya65@gmail.com>
2025-02-03 16:10:42 +0545

    multiple video as playlist

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -10,20 +10,9 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title:const Text("Youtube player Demo")),
-        body:SingleChildScrollView(
-          child: Column(children: [
-            YoutubeVideo(
-                "https://www.youtube.com/watch?v=Ci4t-NkOY3I&list=PLjxrf2q8roU23XGwz3Km7sQZFTdB996iG"),
-
-            ListView.builder(
-                physics: const NeverScrollableScrollPhysics(),
-                shrinkWrap: true,
-                itemCount: 20,
-                itemBuilder: (ctx,i){
-                  return ListTile(title: Text("Item $i"),);
-                })
-          ],),
+    return Scaffold(
+        body:Container(
+          child: YoutubeVideo(),
         ));
   }
 }
--- lib/videoplayer.dart
+++ lib/videoplayer.dart
@@ -1,18 +1,10 @@
-
 import 'package:flutter/material.dart';
-
 import 'package:flutter/services.dart';
 import 'package:visibility_detector/visibility_detector.dart';
 import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 
-
-
-
 class YoutubeVideo extends StatefulWidget {
-  String youtubeUrl;
-
-  YoutubeVideo(this.youtubeUrl);
-
+  YoutubeVideo({super.key});
 
   @override
   _YoutubeVideoState createState() => _YoutubeVideoState();
@@ -25,24 +17,22 @@ class _YoutubeVideoState extends State<YoutubeVideo> {
   late TextEditingController _seekToController;
   bool _isPlayerReady = false;
   late String videoId;
+  final List<String> _ids = [
+    "rzC3pXB0D-c",
+    'Nc6ffPtAtnY',
+    "X8134JVOPb8",
+    'TnwPxwhkTcU',
+    "pkiaP5Ky2iM",
+  ];
 
   @override
   void initState() {
     super.initState();
-    videoId = YoutubePlayer.convertUrlToId(widget.youtubeUrl)!;
+    videoId = _ids.first;
     _controller = YoutubePlayerController(
       initialVideoId: videoId,
-      flags: const YoutubePlayerFlags(
-        mute: false,
-        autoPlay: false,
-        disableDragSeek: false,
-        loop: false,
-        isLive: false,
-        forceHD: false,
-        enableCaption: true,
-      ),
-    )
-      ..addListener(listener);
+      flags: const YoutubePlayerFlags(mute: false, autoPlay: true, disableDragSeek: false, loop: false, isLive: false, forceHD: false, enableCaption: true, controlsVisibleAtStart: false),
+    )..addListener(listener);
     _idController = TextEditingController();
     _seekToController = TextEditingController();
   }
@@ -67,68 +57,74 @@ class _YoutubeVideoState extends State<YoutubeVideo> {
     _seekToController.dispose();
     super.dispose();
   }
-
+PageController _pc=PageController();
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 200,
-      color: Colors.blue,
-      child: VisibilityDetector(
-        key: const Key("unique key"),
-        onVisibilityChanged: (info) {
-          if (info.visibleFraction == 0) {
-            _controller.pause();
-          } else {
-            _controller.value.isPlaying
-                ? _controller.play()
-                : _controller.pause();
+    return PageView(
+      controller: _pc,
+      scrollDirection: Axis.vertical,
+      onPageChanged: (value) {
+        _controller.load(_ids[1]);
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("PLay next video")));
 
-          }
-        },
-        child: YoutubePlayerBuilder(
-          onExitFullScreen: () {
-            // The player forces portraitUp after exiting fullscreen. This overrides the behaviour.
-            SystemChrome.setPreferredOrientations(DeviceOrientation.values);
-          },
-          player: YoutubePlayer(
-
-            controller: _controller,
-            showVideoProgressIndicator: true,
-            progressIndicatorColor: Colors.blueAccent,
-            topActions: <Widget>[
-              const SizedBox(width: 8.0),
-              Expanded(
-                child: Text(
-                  _controller.metadata.title,
-                  style: const TextStyle(
-                    color: Colors.white,
-                    fontSize: 18.0,
-                  ),
-                  overflow: TextOverflow.ellipsis,
-                  maxLines: 1,
-                ),
-              ),
-
-            ],
-            onReady: () {
-              _controller.addListener(listener);
+      },
+      children: [
+        Container(
+          color: Colors.blue,
+          child: VisibilityDetector(
+            key: const Key("unique key"),
+            onVisibilityChanged: (info) {
+              if (info.visibleFraction == 0) {
+                _controller.pause();
+              } else {
+                _controller.value.isPlaying ? _controller.play() : _controller.pause();
+              }
             },
-            onEnded: (data) {},
-          ),
-          builder: (context, player) =>
-              Scaffold(
+            child: YoutubePlayerBuilder(
+              onExitFullScreen: () {
+                // The player forces portraitUp after exiting fullscreen. This overrides the behaviour.
+                SystemChrome.setPreferredOrientations(DeviceOrientation.values);
+              },
+              player: YoutubePlayer(
+                aspectRatio: 9 / 16,
+                controller: _controller,
+                showVideoProgressIndicator: true,
+                progressIndicatorColor: Colors.blueAccent,
+                topActions: <Widget>[
+                  const SizedBox(width: 8.0),
+                  Expanded(
+                    child: Text(
+                      _controller.metadata.title,
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 18.0,
+                      ),
+                      overflow: TextOverflow.ellipsis,
+                      maxLines: 1,
+                    ),
+                  ),
+                ],
+                onReady: () {
+                  _controller.addListener(listener);
+                },
+                onEnded: (data) {
+                  _pc.nextPage(duration: Duration(milliseconds: 5), curve: Curves.bounceIn);
+                  _controller.load(_ids[(_ids.indexOf(data.videoId) + 1) % _ids.length]);
+                  ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("PLay next video")));
+                },
+              ),
+              builder: (context, player) => Scaffold(
                 key: _scaffoldKey,
-                body:
-                ListView(
+                body: ListView(
                   children: [
                     player,
                   ],
                 ),
               ),
+            ),
+          ),
         ),
-      ),
+      ],
     );
   }
-
-
 }
